3baf40a1952cca1626145186f42c5e3bcbaf2a1b
mssmln <crocimassimiliano93@gmail.com>
2023-07-13 10:39:56 +0200

    compiled and tested successfully problem1 method.

--- src/main/scala/bdh/utils/Utils.scala
+++ src/main/scala/bdh/utils/Utils.scala
@@ -4,4 +4,24 @@ class Utils {
 
   final def equalityTwoInt(a: Int, b: Int): Boolean = a == b
 
+  final def problem1(arr: Array[Int], k: Int): Array[Array[Int]] = {
+    var arOfArMatchK = Array.empty[Array[Int]]
+    for (it <- arr) {
+      var arMatchK = Array.empty[Int]
+      for (el <- arr.slice(1, arr.length)) {
+        var result = 0
+        if (arr.indexOf(el) > arr.indexOf(it)) {
+          result = it + el
+        }
+        if (result == k) {
+          arMatchK = arMatchK :+ it
+          arMatchK = arMatchK :+ el
+          arOfArMatchK = arOfArMatchK :+ arMatchK
+        }
+      }
+    }
+    arOfArMatchK
+  }
+
+
 }
--- src/test/scala/bdh/utils/UtilsTest.scala
+++ src/test/scala/bdh/utils/UtilsTest.scala
@@ -3,11 +3,25 @@ import org.scalatest.matchers.should._
 import bdh.utils.Utils
 
 class UtilsTest extends AnyFlatSpec with Matchers {
-  it should "assert the equality of two integers" in {
+  it should "assert the equality of two integers without Matchers" in {
     val a = 3
     val b = 3
     val equalityTwoInt = new Utils().equalityTwoInt(a, b)
+    assert(equalityTwoInt)
+  }
+
+  it should "assert the equality of two integers with Matchers" in {
+    val a = 3
+    val b = 3
     a shouldEqual b
+  }
+
+
+  "The method problem1" should "return a 2D array containing 10 and 7" in {
+    val arr = Array(10, 15, 3, 7)
+    val int = 17
+    val problem1 = new Utils().problem1(arr, int)
+    assert(problem1(0)(0) == 10 && problem1(0)(1) == 7)
   }
 
 }

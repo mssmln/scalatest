db6d678104bafc436619cd153ea47bed5d5ed620
mssmln <crocimassimiliano93@gmail.com>
2023-07-14 13:03:34 +0200

    refactored using shouldBe rather than assert upon the string check

--- src/test/scala/TestReverseString.scala
+++ src/test/scala/TestReverseString.scala
@@ -1,12 +1,16 @@
 import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
 
 
-class TestReverseString extends AnyFlatSpec {
-  ignore should "be reversed" in {
+class TestReverseString extends AnyFlatSpec with Matchers {
+  "The string" should "be reversed" in {
     val string = "hello"
     val reversedString = string.reverse
     val expectedString = "olleh"
-    assert(expectedString == reversedString)
+    //assert(expectedString == reversedString)
+    // shouldBe performs a reference equality check using the eq operator.
+    // It checks if the actual value and the expected value refer to the exact same object in memory
+    expectedString shouldBe reversedString
   }
 
 }
